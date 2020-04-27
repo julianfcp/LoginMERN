6a0d06e4bbb0cd4985ea0384c60d9d9b16c5c434
julianfcp <julian.fcp@gmail.com>
2020-04-27 17:49:57 -0500

    jwt token secret

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const User = require('../model/User');
-const { registerValdation } = require('../validation');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { registerValdation, loginValidation } = require('../validation');
 
-
+// REGISTER
 
 router.post('/register', async (req, res) => {
     const { name, email, password, conf_password } = req.body; // destructuring de los datos que vienen
@@ -40,6 +42,44 @@ router.post('/register', async (req, res) => {
         res.json({success: true, message: 'User Created '});
     }
 });
+
+
+// LOGIN
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body; // destructuring de los datos que vienen
+    const errors = [];
+    // validation
+    const val_error = loginValidation(req.body);
+    const user = await User.findOne({email});
+    
+    // check errors with joi
+    if (val_error){
+        errors.push({success: false, message: val_error.details[0].message});
+    }
+    // check if user exists
+    if (!user) {
+        errors.push({success: false, message: 'Email or password is wrong'});
+    } else {
+        // check is password is wrong
+        const validPass = await bcrypt.compare(password, user.password);    
+        if(!validPass) {
+            errors.push({success: false, message: 'Invalid password!'});
+        }
+    }
+    
+    if (errors.length > 0) {
+        res.json(errors);
+    } else {
+        // Create Token
+        const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+        res.header('auth-token', token).send(token);
+        //res.json({success: true, message: 'Logged in! '});
+    }
+
+});
+
+
 
 
 
